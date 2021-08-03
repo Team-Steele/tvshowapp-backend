6293ed117f52c7f8b0feeb24cf85faa9e745c165
mligon115 <mligon115@yahoo.com>
2021-08-02 20:11:29 -0500

    updated update route

--- tvshowapp-backend/routes/users.Route.js
+++ tvshowapp-backend/routes/users.Route.js
@@ -30,22 +30,76 @@ router.post('/users', (req, res) => {
     })
 })  
 
+// router.put('/users/:id', (req, res) => {
+//     const routeId = req.params.id;
+//   User.findOneAndUpdate({
+//     _id: routeId,
+//   },
+//   {
+//     $addToSet: {
+//         tvShows: req.body.tvShows
+//       }
+//   }, 
+//   {
+//       new: true
+//   }).then((shows) => {
+//     console.log(shows);
+//     res.json(shows);
+//   });
+// });  
+
 router.put('/users/:id', (req, res) => {
-    const routeId = req.params.id;
-  User.findOneAndUpdate({
-    _id: routeId,
-  },
-  {
-    $addToSet: {
-        tvShows: req.body.tvShows
+  const routeId = req.params.id
+  console.log("backend comment", req.body)
+  if (req.body.opinion) {
+     Comments.create({comment: req.body.opinion})
+     .then( comment => {
+        User.findOneAndUpdate(
+          { _id: routeId },
+          {
+            $addToSet: {
+              tvShows: req.body.tvShows,
+            },
+            $addToSet: {
+              opinion: comment._id,
+            },
+          },
+          { new: true }
+        ).then((user) => {
+          console.log(user);
+          res.json(user);
+        });
+     
+     })
+  
+    } else {
+      User.findOneAndUpdate(
+        { _id: routeId },
+        {
+          $addToSet: {
+            tvShows: req.body.tvShows,
+          },
+         },
+        { new: true }
+      ).then((user) => {
+        console.log(user);
+        res.json(user);
+      });
     }
-  }, 
-  {
-      new: true
-  }).then((shows) => {
-    console.log(shows);
-    res.json(shows);
-  });
-});   
+
+})
+
+router.delete('/delete/:id', (req, res) => {
+  const routeId = req.params.id;
+  User.findOneAndRemove(
+    {
+    _id: routeId
+    }
+  )
+  .then(users => {
+    console.log(users)
+    res.json(users)
+  })
+})
 
 module.exports = router;
